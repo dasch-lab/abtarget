e839bd1b72a5882c388d9eaa3e94da0a9a7f1360
Sara Joubbi <s.joubbi@toscanalifesciences.org>
2023-01-12 09:40:39 +0100

    Changed dataset

--- dataset.py
+++ dataset.py
@@ -5,8 +5,22 @@ import re
 import argparse
 import traceback
 import sys
+import numpy as np
+import csv
 
 from src import pdb as pdbParse
+
+def read_files(path):
+  
+  res = []
+  target = path.split('/')[-1].split('_')[2]
+  for filename in os.listdir(path):
+    pdb_path = os.path.join(path, filename)
+    sequence_h = pdbParse.sequence(pdb_path, 'H', True)
+    sequence_l = pdbParse.sequence(pdb_path, 'L', True)
+    res.append([filename, sequence_h, sequence_l, target])
+  
+  return res
 
 if __name__ == "__main__":
 
@@ -15,15 +29,24 @@ if __name__ == "__main__":
   argparser.add_argument('-i', '--input', help='input directory', dest='input', type=str, required=True)
 
   # Parse arguments
-  args = argparser.parse_args()
+  #args = argparser.parse_args()
+  #args.input = '/disk1/mAb_dataset/LH_Protein_Kabat'
+  #input_path = '/disk1/mAb_dataset/LH_Protein_Kabat'
 
-  for filename in os.listdir(args.input):
-    # Check file type
-    # Parse PDB file
-    # Store sequence data  
+  print('Start')
+  protein = read_files('/disk1/abtarget/mAb_dataset/NR_LH_Protein_Kabat')
+  print('Protein: {}'.format(len(protein)))
+  non_protein = read_files('/disk1/abtarget/mAb_dataset/NR_LH_NonProtein_Kabat')
+  print('Non Protein: {}'.format(len(non_protein)))
+  
+  details = [['File_name', 'VH', 'VL', 'target']]
 
-    # Extract data from RCSB
-    for chain in ['H', 'L']:
-      sequence = pdbParse.sequence(pdb_path, chain, True)
-
-    pass
+  print('Start writing')
+  with open('/disk1/abtarget/mAb_dataset/dataset.csv', 'w') as f:
+    mywriter = csv.writer(f, delimiter=',')
+    mywriter.writerows(details)
+    mywriter.writerows(protein)
+    print('Protein')
+    mywriter.writerows(non_protein)
+    print('Non-Protein')
+    
