87014bc57337105175b446d0e41537ce5a8842ee
Sara Joubbi <s.joubbi@toscanalifesciences.org>
2024-08-10 11:55:16 +0200

    review

--- train_classic_ml.py
+++ train_classic_ml.py
@@ -17,6 +17,9 @@ from src.protbert import BaselineOne
 from src.baseline_dataset import SAbDabDataset
 from src.metrics import MCC
 from src.training_eval import final_score_eval, confusion_matrix
+from sklearn.linear_model import Lasso
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 def embedding_phase(dataloaders, phase, model):
 
@@ -41,9 +44,7 @@ def embedding_phase(dataloaders, phase, model):
 
 
 
-
-
-def stratified_split1(dataset1 : torch.utils.data.Dataset, dataset2 : torch.utils.data.Dataset, labels1, labels2, train_size, tot):
+def stratified_split1(dataset1 : torch.utils.data.Dataset, dataset2 : torch.utils.data.Dataset, dataset3 : torch.utils.data.Dataset, labels1, labels2, labels3, train_size, tot):
 
   '''
   Split the dataset proportionally according to the sample label
@@ -53,19 +54,23 @@ def stratified_split1(dataset1 : torch.utils.data.Dataset, dataset2 : torch.util
   classList = list(set(labels1))
   resultList = {
     'train': [],
+    'val': [],
     'test': []
   }
 
   classData1 = {}
   classData2 = {}
+  classData3 = {}
   for name in classList:
 
     # Get subsample of indexes for this class
     classData1[name] = [ idx for idx, label in enumerate(labels1) if label == name ]
     classData2[name] = [ idx for idx, label in enumerate(labels2) if label == name ]
+    classData3[name] = [ idx for idx, label in enumerate(labels3) if label == name ]
 
   classStats = {
     'train':{},
+    'val':{},
     'test': {}
   }
   for name in classList:
@@ -82,26 +87,31 @@ def stratified_split1(dataset1 : torch.utils.data.Dataset, dataset2 : torch.util
         trainList = classData1[name]
 
     #trainList = classData1[name]
-    testList = classData2[name]
+    valList = classData2[name]
+    testList = classData3[name]
     
+    print(len(valList))
     print(len(trainList))
 
     # Update stats
     classStats['test'][name] = len(testList)
+    classStats['val'][name] = len(valList)
     classStats['train'][name] = len(trainList)
 
     # Concatenate indexes
     resultList['test'].extend(testList)
+    resultList['val'].extend(valList)
     resultList['train'].extend(trainList)
       
 
   train_data = torch.utils.data.Subset(dataset1, resultList['train'])
-  test_data = torch.utils.data.Subset(dataset2, resultList['test'])
+  val_data = torch.utils.data.Subset(dataset2, resultList['val'])
+  test_data = torch.utils.data.Subset(dataset3, resultList['test'])
 
-  return train_data, test_data
+  return train_data, val_data, test_data
 
 
-def controlled_split(dataset1 : torch.utils.data.Dataset, dataset2 : torch.utils.data.Dataset, labels1, labels2, subset, proportion, fraction):
+def controlled_split(dataset1 : torch.utils.data.Dataset, dataset2 : torch.utils.data.Dataset, dataset3 : torch.utils.data.Dataset, labels1, labels2, labels3, subset, proportion, fraction):
 
   '''
   Split the dataset proportionally according to the sample label
@@ -112,16 +122,19 @@ def controlled_split(dataset1 : torch.utils.data.Dataset, dataset2 : torch.utils
   classList = list(set(labels1))
   resultList = {
     'test': [],
+    'val':[],
     'train': []
   }
 
   classData1 = {}
   classData2 = {}
+  classData3 = {}
 
   for name in classList:
     # Get subsample of indexes for this class
     classData1[name] = [ idx for idx, label in enumerate(labels1) if label == name ]
     classData2[name] = [ idx for idx, label in enumerate(labels2) if label == name ]
+    classData3[name] = [ idx for idx, label in enumerate(labels3) if label == name ]
 
   # Get shorter element
   shorter_class = min(classData1.items(), key=lambda x: len(x[1]))[0]
@@ -140,6 +153,7 @@ def controlled_split(dataset1 : torch.utils.data.Dataset, dataset2 : torch.utils
 
   classStats = {
     'train': {},
+    'val':{},
     'test': {}
   }
 
@@ -165,15 +179,18 @@ def controlled_split(dataset1 : torch.utils.data.Dataset, dataset2 : torch.utils
           classDatasubset.append(trainList_tot[base:base+train_size])
 
       trainList =  classDatasubset[subset]
-      testList = classData2[name]
+    valList = classData2[name]
+    testList = classData3[name]
       
 
     # Update stats
     classStats['train'][name] = len(trainList)
+    classStats['val'][name] = len(valList)
     classStats['test'][name] = len(testList)
 
     # Concatenate indexes
     resultList['train'].extend(trainList)
+    resultList['val'].extend(valList)
     resultList['test'].extend(testList)
 
   # Shuffle index lists
@@ -185,15 +202,10 @@ def controlled_split(dataset1 : torch.utils.data.Dataset, dataset2 : torch.utils
   
   # Construct the test and train datasets
   train_data = torch.utils.data.Subset(dataset1, resultList['train'])
-  test_data = torch.utils.data.Subset(dataset2, resultList['test'])
+  val_data = torch.utils.data.Subset(dataset2, resultList['val'])
+  test_data = torch.utils.data.Subset(dataset3, resultList['test'])
 
-      
-  # Save validation split in a txt file
-  #with open('/disk1/abtarget/dataset/split/test.txt','w') as file:
-  #  file.write("\n".join(str(item) for item in resultList['test']))
-  #  #data.write(str(dictionary))
-
-  return train_data, test_data
+  return train_data, val_data, test_data
 
 def controlled_split_kcross(dataset1 : torch.utils.data.Dataset, dataset2 : torch.utils.data.Dataset, labels1, labels2, subset, proportion, fraction):
 
@@ -289,56 +301,18 @@ def controlled_split_kcross(dataset1 : torch.utils.data.Dataset, dataset2 : torc
   return train_data, test_data
 
 
-def train_OCSVM(model, dataloaders):
-  since = time.time()
 
-  # Epoch train and validation phase
-  model.eval()
-
-  labels, embeddings =  embedding_phase(dataloaders, "train", model)
-
-  #print('umap')
-  #reducer = umap.UMAP()
-  #embedding = reducer.fit_transform(embeddings)
-  #embedding.shape
-
-  #df_test = pd.DataFrame(embeddings, columns=['feature1', 'feature2'])
-  #df_test['y_test'] = labels
-  #plt.scatter(df_test['feature1'], df_test['feature2'], c=df_test['y_test'], cmap='rainbow')
-  
-  print('OCSVM')
-  #nu = 0.15
-  nu = 0.07
-  #nu = 0.5
-  print('Start One class')
-  one_class_svm = OneClassSVM(nu = nu, kernel = 'rbf', gamma = 'auto').fit(embeddings)
-  print('One class finished')
-
-  return one_class_svm
-
-
-def eval_OCSVM(one_class_svm):
+def eval(classifier, name, args):
   print('Start Eval - embedding')
-  labels, embeddings =  embedding_phase(dataloaders, "test", model)
+  labels, embeddings =  embedding_phase(dataloaders, name, model)
 
-  reducer = umap.UMAP()
-  embedding = reducer.fit_transform(embeddings)
-  embedding.shape
+  prediction = classifier.predict(embeddings)
 
-  print('save results')
-  plt.scatter(embedding[:, 0], embedding[:, 1],c=[sns.color_palette()[x] for x in labels])
-  plt.gca().set_aspect('equal', 'datalim')
-  plt.title('UMAP projection', fontsize=24)
-  plt.savefig('umap.jpg')
-
-  prediction = one_class_svm.predict(embeddings)
-  prediction = [1 if i==-1 else 0 for i in prediction]
-  print(classification_report(labels, prediction))
-
-  plt.scatter(embedding[:, 0], embedding[:, 1],c=[sns.color_palette()[x] for x in prediction])
-  plt.gca().set_aspect('equal', 'datalim')
-  plt.title('OCSVM UMAP projection', fontsize=24)
-  plt.savefig('ocsvm.jpg')
+  if args.ocsvm:
+    prediction = np.where(prediction == -1, 1, np.where(prediction == 1, 0, prediction))
+  
+  if args.lasso:
+    prediction =  prediction.round()
 
   return labels, prediction
 
@@ -359,11 +333,17 @@ if __name__ == "__main__":
   argparser.add_argument('-o', '--optimizer', type=str, help='Optimizer: SGD or Adam', default='Adam')
   argparser.add_argument('-l', '--lr', type=float, help='Learning rate', default=3e-5)
   argparser.add_argument('-cr', '--criterion', type=str, help='Criterion: BCE or Crossentropy', default='Crossentropy')
-  argparser.add_argument('-en', '--ensemble', type=bool, help='Ensemble model', default= False)
+  argparser.add_argument('-en', '--ensemble', type=bool, help='Ensemble model', default= True)
   argparser.add_argument('-tr', '--pretrain', type=bool, help='Freeze encoder', default= True)
   argparser.add_argument('-sub', '--subset', type=int, help='Subset to train the model with', default = 7)
   argparser.add_argument('-train', '--training', type=str, help='Training dataset', default = "/disk1/abtarget/dataset/sabdab/split/sabdab_200423_train1_norep.csv")
+  argparser.add_argument('-val', '--val', type=str, help='Validation dataset', default = "/disk1/abtarget/dataset/sabdab/split/sabdab_200423_val_norep.csv")
   argparser.add_argument('-test', '--test', type=str, help='Test dataset', default = "/disk1/abtarget/dataset/sabdab/split/sabdab_200423_test_norep.csv")
+  argparser.add_argument('-lasso', '--lasso', type=bool, help='LASSO classification', default = False)
+  argparser.add_argument('-rf', '--rf', type=bool, help='Random Forest classification', default = False)
+  argparser.add_argument('-knn', '--knn', type=bool, help='KNN classification', default = False)
+  argparser.add_argument('-ocsvm', '--ocsvm', type=bool, help='OCSVM classification', default = True)
+
 
     
 
@@ -385,7 +365,8 @@ if __name__ == "__main__":
   #dataset =  SAbDabDataset('/disk1/abtarget/dataset/sabdab/split/sabdab_200423_train_norep.csv')
 
   dataset1 = SAbDabDataset(args.training)
-  dataset2 = SAbDabDataset(args.test)
+  dataset2 = SAbDabDataset(args.val)
+  dataset3 = SAbDabDataset(args.test)
   
 
   if args.threads:
@@ -399,9 +380,9 @@ if __name__ == "__main__":
 
   if (args.ensemble):
     subset = args.subset
-    train_data, test_data = controlled_split(dataset1, dataset2, dataset1.labels, dataset2.labels, fraction=nn_train, subset = subset, proportion=0.5)
+    train_data, val_data, test_data = controlled_split(dataset1, dataset2, dataset3, dataset1.labels, dataset2.labels,  dataset3.labels, fraction=nn_train, subset = subset, proportion=0.5)
   else:
-    train_data, test_data = stratified_split1(dataset1, dataset2, dataset1.labels, dataset2.labels, train_size=10000, tot = True)
+    train_data, val_data, test_data = stratified_split1(dataset1, dataset2, dataset3, dataset1.labels, dataset2.labels,  dataset3.labels, train_size=10000, tot = True)
     
     
   # Train and Test Dataloaders - (Wrap data with appropriate data loaders)
@@ -409,6 +390,9 @@ if __name__ == "__main__":
     train_data, batch_size=args.batch_size, shuffle=True, num_workers=4, pin_memory=True
   )
   val_loader = torch.utils.data.DataLoader(
+    val_data, batch_size=args.batch_size, num_workers=4, pin_memory=True
+  )
+  test_loader = torch.utils.data.DataLoader(
     test_data, batch_size=args.batch_size, num_workers=4, pin_memory=True
   )
 
@@ -418,8 +402,9 @@ if __name__ == "__main__":
 
   # Train script
   dataloaders = {
-    "train": train_loader, 
-    "test": val_loader
+    "train": train_loader,
+    "val": val_loader, 
+    "test": test_loader
   }
 
   # Select model
@@ -427,8 +412,42 @@ if __name__ == "__main__":
   print(model_name)
   model = BaselineOne(args.batch_size, device, nn_classes=args.n_class, freeze_bert=args.pretrain, model_name=args.model)
 
-  one_class_svm = train_OCSVM(model, dataloaders)
-  labels, predictions = eval_OCSVM(one_class_svm)
+  model.eval()
 
-  confusion_matrix(labels, predictions)
+  labels, embeddings =  embedding_phase(dataloaders, "train", model)
+
+  if args.lasso:
+    classifier = Lasso(alpha = 0.1).fit(embeddings,labels)
+  elif args.knn:
+    k = 5
+    classifier = KNeighborsClassifier(n_neighbors=k, weights='distance').fit(embeddings,labels)
+  elif args.rf:
+    estimators = 100
+    classifier = RandomForestClassifier(n_estimators=estimators, criterion="entropy").fit(embeddings,labels)
+  else:
+    if args.ensemble:
+      nu = 0.5
+    else:
+      nu = 0.07
+    classifier = OneClassSVM(nu = nu, kernel = 'rbf', gamma = 'auto').fit(embeddings)
+
+  
+  print('Training')
+  prediction = classifier.predict(embeddings)
+  if args.lasso: 
+    prediction = prediction.round()
+  if args.ocsvm:
+    prediction = np.where(prediction == -1, 1, np.where(prediction == 1, 0, prediction))
+  
+  final_score_eval(labels, prediction)
+
+  print('Validation')
+  labels, predictions = eval(classifier, 'val', args)
   final_score_eval(labels, predictions)
+
+  print('Test')
+  labels, predictions = eval(classifier, 'test', args)
+  final_score_eval(labels, predictions)
+
+  #confusion_matrix(labels, predictions)
+  
